f279fcf70516d2df9ee6b2487a665513bcd7ca0e
Panieri <Alunno@PC-1-INF4>
2017-11-07 12:49:53 +0100

    Aggiunta una classe di test per il CommandManager, ignoratela per ora

---
+++ GruppoChat/Server/Main/Test_CommandManager_main.java
@@ -0,0 +1,11 @@
+//QUESTO E' UN TEST, NON CONINUATE IL SUO SVILUPPO E NON IMPLEMENTATELO
+package Main;
+
+public class Test_CommandManager_main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
