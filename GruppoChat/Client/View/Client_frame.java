67c0ac39fca94a4267b0e4751546c715d467c28d
Francesca <Alunno@PC-3-INF4>
2017-11-07 12:47:24 +0100

    Nuova grafica client

--- GruppoChat/Client/View/Client_frame.java
+++ GruppoChat/Client/View/Client_frame.java
@@ -12,16 +12,17 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Client_frame extends JFrame {
 
 	private JPanel contentPane;
-	private JLabel lblBenvenuto;
-	private JLabel lblComeTiVuoi;
-	private JTextField txtNick;
-	private JComboBox cmbChat;
-	private JLabel lblInCheChat;
-	private JButton btnEntra;
+	private JTextField username;
+	private JTextField textField;
 
 	
 	/**
@@ -35,39 +36,87 @@ public class Client_frame extends JFrame {
 		}
 		setTitle("Bla Bla Chat");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 364);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		lblBenvenuto = new JLabel("Benvenuto!\r\n\r\n");
-		lblBenvenuto.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		lblBenvenuto.setBounds(10, 11, 414, 54);
-		contentPane.add(lblBenvenuto);
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 434, 326);
+		contentPane.add(tabbedPane);
 		
-		lblComeTiVuoi = new JLabel("Come ti vuoi chiamare?");
-		lblComeTiVuoi.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblComeTiVuoi.setBounds(10, 76, 206, 54);
-		contentPane.add(lblComeTiVuoi);
+		JPanel Welcome = new JPanel();
+		tabbedPane.addTab("Benvenuto", null, Welcome, null);
+		Welcome.setLayout(null);
 		
-		txtNick = new JTextField();
-		txtNick.setBounds(214, 88, 210, 35);
-		contentPane.add(txtNick);
-		txtNick.setColumns(10);
+		JLabel lblBenvenuto = new JLabel("Benvenuto");
+		lblBenvenuto.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 22));
+		lblBenvenuto.setBounds(141, 11, 197, 26);
+		Welcome.add(lblBenvenuto);
 		
-		cmbChat = new JComboBox();
-		cmbChat.setBounds(214, 164, 210, 35);
-		contentPane.add(cmbChat);
+		JLabel lblComeTiVuoi = new JLabel("Come ti vuoi chiamare?");
+		lblComeTiVuoi.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblComeTiVuoi.setBounds(26, 92, 206, 14);
+		Welcome.add(lblComeTiVuoi);
 		
-		lblInCheChat = new JLabel("In che chat vuoi entrare?");
-		lblInCheChat.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblInCheChat.setBounds(10, 152, 199, 54);
-		contentPane.add(lblInCheChat);
+		username = new JTextField();
+		username.setBounds(251, 91, 168, 20);
+		Welcome.add(username);
+		username.setColumns(10);
 		
-		btnEntra = new JButton("Entra");
-		btnEntra.setBounds(160, 228, 89, 23);
-		contentPane.add(btnEntra);
+		JLabel lblInCheChat = new JLabel("In che chat vuoi entrare?");
+		lblInCheChat.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblInCheChat.setBounds(26, 158, 197, 14);
+		Welcome.add(lblInCheChat);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(251, 157, 168, 20);
+		Welcome.add(comboBox);
+		
+		JButton btnEntra = new JButton("ENTRA");
+		btnEntra.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnEntra.setBounds(174, 222, 89, 23);
+		Welcome.add(btnEntra);
+		
+		JPanel Chat = new JPanel();
+		tabbedPane.addTab("Chat", null, Chat, null);
+		Chat.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 221, 409, 20);
+		Chat.add(textField);
+		textField.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 36, 409, 174);
+		Chat.add(scrollPane);
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		
+		JButton btnNewButton = new JButton("Muta\r\n");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(42, 264, 89, 23);
+		Chat.add(btnNewButton);
+		
+		JButton btnInvia = new JButton("Invia");
+		btnInvia.setBounds(311, 264, 89, 23);
+		Chat.add(btnInvia);
+		
+		JButton btnEsci = new JButton("Esci");
+		btnEsci.setBounds(171, 264, 89, 23);
+		Chat.add(btnEsci);
+		
+		JLabel lblIniziaAChattare = new JLabel("INIZIA A CHATTARE CON I TUOI AMICI");
+		lblIniziaAChattare.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblIniziaAChattare.setBounds(73, 11, 376, 14);
+		Chat.add(lblIniziaAChattare);
+		
+		
 		
 		this.setVisible(true);
 	}
