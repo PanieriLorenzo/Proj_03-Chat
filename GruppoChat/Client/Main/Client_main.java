2501e499138d4df551885d2f8cadc6a3bc55008e
Panieri <Alunno@PC-3-INF4>
2017-11-15 13:02:59 +0100

    handshake lato client (non finito)

--- GruppoChat/Client/Main/Client_main.java
+++ GruppoChat/Client/Main/Client_main.java
@@ -1,9 +1,10 @@
 package Main;
 
-import View.Client_frame;
+import Control.Control_Client;
+import View.Client_Frame;
 
 public class Client_main {
 	public static void main(String[] args){
-		Client_frame f = new Client_frame();
+		Control_Client c = new Control_Client();
 	}
 }
